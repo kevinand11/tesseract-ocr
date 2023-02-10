3423890444a0ef48a6417b36d29022f71854d12f
kevinand11 <kevinfizu@gmail.com>
2023-02-10 20:33:56 +0100

    deploy script update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,6 @@
-const execa = require('execa')
-const fs = require('fs');
+import execa from 'execa'
+import fs from 'fs'
+
 (async () => {
 	try {
 		await execa('git', ['checkout', '-B', 'gh-pages'])
